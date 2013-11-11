83459db304241df4f783c07a514408c90ab01135
Matthew Caruana Galizia <mattcg@gmail.com>
2013-11-10 18:29:06 -0600

    Subthemes from newer years weren't being matched

--- scripts/sql/subtemas.sql
+++ scripts/sql/subtemas.sql
@@ -1,7 +1,7 @@
 SELECT
 	`2007a2012`.`Año` AS `año`,
 	SUBSTRING_INDEX(`2007a2012`.`14.d. Sector CAD`, ' - ', 1) AS `CAD`,
-	`2007a2012`.`14.a. Sector CRS` AS `CRS`,
+	SUBSTRING_INDEX(`2007a2012`.`14.a. Sector CRS`, ' - ', 1) AS `CRS`,
 	COUNT(`CAD`) AS `num`,
 	SUM(REPLACE(`2007a2012`.`13. Des AOD (€)`,',','')) AS `total`,
 	SUM(IF(`2007a2012`.`13. Des AOD (€)` < 0, 0, REPLACE(`2007a2012`.`13. Des AOD (€)`,',',''))) AS `deuda`,
@@ -9,7 +9,7 @@ SELECT
 	`temas_códigos`.`nombre` AS `nombre`
 FROM `2007a2012`
 LEFT JOIN `temas_códigos` ON
-	`2007a2012`.`14.a. Sector CRS` = `temas_códigos`.`CRS`
+	SUBSTRING_INDEX(`2007a2012`.`14.a. Sector CRS`, ' - ', 1) = `temas_códigos`.`CRS`
 WHERE `nombre` IS NOT NULL
 GROUP BY `año`, `CRS`
 ORDER BY `año`, `CRS`;
